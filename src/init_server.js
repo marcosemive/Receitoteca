09b59b76b6613c071ec0bc5eb249c1af6fc5b00b
josleda <jose.leda@academico.ifpb.edu.br>
2026-04-25 19:28:22 +0000

    versão finalizada para entrega 27/04

--- src/init_server.js
+++ src/init_server.js
@@ -4,7 +4,7 @@ import route from './routes.js';
 
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 
 app.use(express.static('public'));
 
@@ -12,6 +12,9 @@ app.use(express.json());
 
 app.use('/api', route);
 
+app.get('/', (req, res) => {
+  res.redirect('/paginainicial.html');
+});
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
 
