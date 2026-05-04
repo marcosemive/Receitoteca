f35178245a5357c4e455c24d2a958e38289356c7
josleda <jose.leda@academico.ifpb.edu.br>
2026-05-04 14:34:47 +0000

    reestruturação dos arquivos para implementação de seeders - poucas alterações

---
+++ src/database/load-seeders.js
@@ -0,0 +1,1 @@
+(equivalente a seeders .js)
---
+++ src/models/receitas.js
@@ -0,0 +1,3 @@
+arquivo de escrever as coisas lá que vao ser importadas nas rotas
+
+provavelmente é necessario realizar algumas adaptacoes, visto que estamos utilizando o arquivo .js e nao .json
