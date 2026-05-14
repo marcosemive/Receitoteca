c95095d5fbb9e2fd82fe1a28042d29fff5a353ea
Marcos Vinícius <zmr.storm@gmail.com>
2026-05-14 11:29:21 +0000

    criacao de mais tabelas no migration --> verificar tipos

--- src/database/migration.js
+++ src/database/migration.js
@@ -16,9 +16,77 @@ async function up() {
       steps TEXT NOT NULL
     )
   `;
- 
+    
   await db.run(receitasSql);
-}
+
+  const usuarioSql = `
+    CREATE TABLE usuario (
+      id_usuario SERIAL PRIMARY KEY,
+      nome VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL UNIQUE,
+      senha VARCHAR(50) NOT NULL
+    )
+  `;
+
+  await db.run(usuarioSql);
+
+
+  const chefSql = `
+    CREATE TABLE chef (
+      id_chef SERIAL PRIMARY KEY,
+      nome VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL UNIQUE,
+      senha VARCHAR(50) NOT NULL
+    )
+  `;
+
+  await db.run(chefSql);
+
+
+  const favoritoSql = `
+    CREATE TABLE favorito (
+      id_usuario_fk INTEGER NOT NULL,
+      id_receita_fk INTEGER NOT NULL,
+      PRIMARY KEY (id_usuario_fk, id_receita_fk),
+      FOREIGN KEY (id_usuario_fk)
+          REFERENCES usuario(id_usuario)
+          ON DELETE CASCADE,
+      FOREIGN KEY (id_receita_fk)
+          REFERENCES receita(id_receita)
+          ON DELETE CASCADE
+    )
+  `;
+
+  await db.run(favoritoSql);
+
+  const etiquetaSql = `
+    CREATE TABLE etiqueta (
+      id_etiqueta SERIAL PRIMARY KEY,
+      nome VARCHAR(50) NOT NULL UNIQUE
+    )
+  `;
+
+  await db.run(etiquetaSql);
+
+  const receita_etiqueta = `
+    CREATE TABLE receita_etiqueta (
+      id_receita_fk INTEGER NOT NULL,
+      id_etiqueta_fk INTEGER NOT NULL,
+      PRIMARY KEY (id_receita_fk, id_etiqueta_fk),
+      FOREIGN KEY (id_receita_fk)
+          REFERENCES receita(id_receita)
+          ON DELETE CASCADE,
+      FOREIGN KEY (id_etiqueta_fk)
+          REFERENCES etiqueta(id_etiqueta)
+          ON DELETE CASCADE
+    )
+  `;
+  
+  await db.run(receita_etiqueta);
+
+}  
+
+
  
 export default { up };
  
